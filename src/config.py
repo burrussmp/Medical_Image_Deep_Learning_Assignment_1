974ab718dd4234acd84c16fba26ca6d28dc90bc6
Matthew Burruss <burrussmatthew@gmail.com>
2020-01-11 14:10:25 -0600

    config for colab

--- src/config.py
+++ src/config.py
@@ -1,5 +1,10 @@
 
-PATH_TO_LABELS = './Medical_Image_Deep_Learning_Assignment_1/labels/labels.txt'
-PATH_TO_VALIDATION_FOLDER = './Medical_Image_Deep_Learning_Assignment_1/validation'
-PATH_TO_TEST_FOLDER = './Medical_Image_Deep_Learning_Assignment_1/test'
-PATH_TO_TRAIN_FOLDER = './Medical_Image_Deep_Learning_Assignment_1/train'
+# PATH_TO_LABELS = './Medical_Image_Deep_Learning_Assignment_1/labels/labels.txt'
+# PATH_TO_VALIDATION_FOLDER = './Medical_Image_Deep_Learning_Assignment_1/validation'
+# PATH_TO_TEST_FOLDER = './Medical_Image_Deep_Learning_Assignment_1/test'
+# PATH_TO_TRAIN_FOLDER = './Medical_Image_Deep_Learning_Assignment_1/train'
+
+PATH_TO_LABELS = '../labels/labels.txt'
+PATH_TO_VALIDATION_FOLDER = '../validation'
+PATH_TO_TEST_FOLDER = '../test'
+PATH_TO_TRAIN_FOLDER = '../train'
